4cf2b5fa2603eb91ecedb744688006b57106e0fb
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-01-31 16:21:55 +1030

    feat: add intro and about me cards

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const LINKS = {
+  github: 'https://github.com/phoenixpereira',
+  linkedin: 'https://www.linkedin.com/in/phoenixpereira/',
+}
+
+export const loaderAnimation =
+    ['.loader', {opacity: [1, 0], pointerEvents: 'none'}, {easing: 'ease-out'}]
