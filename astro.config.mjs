9d7f5ad507d3da7542547dc6df9bbe0e80e4f5b0
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-02-01 12:04:07 +1030

    chore: update astro.config.mjs with new imports and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-
-import react from "@astrojs/react";
+import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://phoenixpereira.github.io'
 });
