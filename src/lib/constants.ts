d1ae3b99a558fad42a037ba01e7af946bbe236d0
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-02-26 14:04:12 +1030

    chore: update email

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,11 @@
 export const LINKS = {
-  email: 'mailto:phoenixpereira99@gmail.com',
-  linkedin: 'https://www.linkedin.com/in/phoenixpereira/',
-  github: 'https://github.com/phoenixpereira',
+	email: 'mailto:phoenixpereira@outlook.com.au',
+	linkedin: 'https://www.linkedin.com/in/phoenixpereira/',
+	github: 'https://github.com/phoenixpereira'
 }
 
-export const loaderAnimation =
-    ['.loader', {opacity: [1, 0], pointerEvents: 'none'}, {easing: 'ease-out'}]
+export const loaderAnimation = [
+	'.loader',
+	{ opacity: [1, 0], pointerEvents: 'none' },
+	{ easing: 'ease-out' }
+]
